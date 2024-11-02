04d8da5bd282f101bc80ce2aca31ece15d5d3f6f
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-11-02 14:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function SportsCar(make, model, topSpeed) {
 	this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype.getTopSpeed = function () {
-	return `${this.topSpeed}`;
-}
+// Set up inheritance properly so SportsCar inherits from Car
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-console.log(car.getMakeModel);
-console.log(car.getTopSpeed);
+SportsCar.prototype.getTopSpeed = function () {
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
