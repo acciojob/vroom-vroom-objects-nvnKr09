deaceef62db1929761662529e0196701aae8c5d4
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-11-02 12:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ SportsCar.prototype.getTopSpeed = function () {
 	return `${this.topSpeed}`;
 }
 
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel);
+console.log(car.getTopSpeed);
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
